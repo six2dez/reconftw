dcd76aaff1bce49a8c4c3b9a606e8730445f058f
Siddhesh Parab <64590875+detonxx@users.noreply.github.com>
2021-01-22 23:34:54 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -105,9 +105,7 @@ sudo mv findomain-linux /usr/local/bin/findomain
 sudo chmod 755 /usr/local/bin/findomain
 cd $dir/massdns; eval make $DEBUG_STD
 sudo cp $dir/massdns/bin/massdns /usr/bin/
-sudo pip3 install mmh3==2.5.1
 
-eval find $dir -name 'requirements.txt' -exec pip3 install --user -r {} \; $DEBUG_STD
 cd $dir/Interlace && sudo python3 setup.py install
 cd $dir/LinkFinder && python3 setup.py install
 cd $dir
@@ -122,6 +120,7 @@ eval wget -O resolvers.txt https://raw.githubusercontent.com/janmasarik/resolver
 eval wget -O permutations_list.txt https://gist.githubusercontent.com/six2dez/ffc2b14d283e8f8eff6ac83e20a3c4b4/raw/137bb6b60c616552c705e93a345c06cec3a2cb1f/permutations_list.txt $DEBUG_STD
 eval wget -O ssrf.py https://gist.githubusercontent.com/h4ms1k/adcc340495d418fcd72ec727a116fea2/raw/ea0774de5e27f9bc855207b175249edae2e9ccef/asyncio_ssrf.py $DEBUG_STD
 eval wget -O all_requirements.txt https://gist.githubusercontent.com/detonxx/92118db85d97f6edb54a0a427ae96a2e/raw/95c0517bdcd1467e9a82992097b7c3e66afccfab/all_requirements.txt $DEBUG_STD
+eval pip3 install -r $dir/all_requirements.txt $DEBUG_STD
 
 printf "${yellow} Remember set your api keys:\n - amass (~/.config/amass/config.ini)\n - subfinder (~/.config/subfinder/config.yaml)\n - git-hound (~/.githound/config.yml)\n - github-endpoints.py ($tools/.github_tokens or GITHUB_TOKEN env var)\n - favup (shodan init SHODANPAIDAPIKEY)\n - SSRF Server (COLLAB_SERVER env var) ${reset}\n"
 
