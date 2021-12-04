da28b009e876aad5ca41bc2fde176a246709f560
six2dez <alexis6307@gmail.com>
2021-12-05 00:37:53 +0100

    Close #405

--- install.sh
+++ install.sh
@@ -292,7 +292,6 @@ eval $SUDO chmod 755 /usr/local/bin/ppfuzz
 eval $SUDO strip -s /usr/local/bin/ppfuzz $DEBUG_STD
 eval $SUDO chmod +x $tools/uDork/uDork.sh
 eval subfinder $DEBUG_STD
-eval subfinder $DEBUG_STD
 
 printf "${bblue}\n Running: Downloading required files ${reset}\n\n"
 ## Downloads
