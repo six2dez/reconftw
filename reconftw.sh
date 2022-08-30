406d2fde005cd38e9bdeb5c7468aeac4e0881248
six2dez <six2dez@gmail.com>
2022-08-30 16:49:50 +0200

    subdomains search by NOERROR DNS response

--- reconftw.sh
+++ reconftw.sh
@@ -295,6 +295,7 @@ function subdomains_full(){
 		sub_passive
 		sub_crt
 		sub_active
+		sub_noerror
 		sub_brute
 		sub_permut
 		sub_recursive_passive
@@ -406,6 +407,33 @@ function sub_active(){
 		end_subfunc "${NUMOFLINES} subs DNS resolved from passive" ${FUNCNAME[0]}
 	else
 		printf "${yellow} ${FUNCNAME[0]} is already processed, to force executing ${FUNCNAME[0]} delete\n    $called_fn_dir/.${FUNCNAME[0]} ${reset}\n\n"
+	fi
+}
+
+function sub_noerror(){
+	if { [ ! -f "$called_fn_dir/.${FUNCNAME[0]}" ] || [ "$DIFF" = true ]; } && [ "$SUBNOERROR" = true ]; then
+		start_subfunc ${FUNCNAME[0]} "Running : Checking NOERROR DNS response"
+		if [[ $(echo "${RANDOM}thistotallynotexist${RANDOM}.$domain" | dnsx -r ~/Tools/resolvers.txt -rcode noerror,nxdomain -retry 3 -silent | cut -d' ' -f2) == "[NOERROR]" ]]; then 
+			resolvers_update_quick_local
+			if [ "$DEEP" = true ]; then
+				dnsx -d $domain -r $resolvers -silent -rcode noerror -w $subs_wordlist_big | cut -d' ' -f1 | anew -q .tmp/subs_noerror.txt 2>>"$LOGFILE" &>/dev/null
+			else
+				dnsx -d $domain -r $resolvers -silent -rcode noerror -w $subs_wordlist | cut -d' ' -f1 | anew -q .tmp/subs_noerror.txt 2>>"$LOGFILE" &>/dev/null
+			fi
+			if [ "$INSCOPE" = true ]; then
+				check_inscope .tmp/subs_noerror.txt
+			fi
+			NUMOFLINES=$(cat .tmp/subs_noerror.txt 2>>"$LOGFILE" | sed "s/*.//" | grep ".$domain$" | anew subdomains/subdomains.txt | sed '/^$/d' | wc -l)
+			end_subfunc "${NUMOFLINES} new subs (DNS noerror)" ${FUNCNAME[0]}
+		else 
+			printf "\n${yellow} Detected DNSSEC black lies, skipping this technique ${reset}\n" 
+		fi
+	else
+		if [ "$SUBBRUTE" = false ]; then
+			printf "\n${yellow} ${FUNCNAME[0]} skipped in this mode or defined in reconftw.cfg ${reset}\n"
+		else
+			printf "${yellow} ${FUNCNAME[0]} is already processed, to force executing ${FUNCNAME[0]} delete\n    $called_fn_dir/.${FUNCNAME[0]} ${reset}\n\n"
+		fi
 	fi
 }
 
