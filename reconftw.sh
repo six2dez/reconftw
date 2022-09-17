743db0fd479516b48f7c4e0b4256e9607b224633
Nico Kokonas <nico@nicomee.com>
2022-09-17 15:28:11 -0600

    sendToNotify handles big file uploads

--- reconftw.sh
+++ reconftw.sh
@@ -1932,21 +1932,7 @@ function sendToNotify {
 		if [[ -z "$NOTIFY_CONFIG" ]]; then
 			NOTIFY_CONFIG=~/.config/notify/provider-config.yaml
 		fi
-		if grep -q '^ telegram\|^telegram\|^    telegram' $NOTIFY_CONFIG ; then
-			notification "Sending ${domain} data over Telegram" info
-			telegram_chat_id=$(cat ${NOTIFY_CONFIG} | grep '^    telegram_chat_id\|^telegram_chat_id\|^    telegram_chat_id' | xargs | cut -d' ' -f2)
-			telegram_key=$(cat ${NOTIFY_CONFIG} | grep '^    telegram_api_key\|^telegram_api_key\|^    telegram_apikey' | xargs | cut -d' ' -f2 )
-			curl -F document=@${1} "https://api.telegram.org/bot${telegram_key}/sendDocument?chat_id=${telegram_chat_id}" &>/dev/null
-		fi
-		if grep -q '^ discord\|^discord\|^    discord' $NOTIFY_CONFIG ; then
-			notification "Sending ${domain} data over Discord" info
-			discord_url=$(cat ${NOTIFY_CONFIG} | grep '^ discord_webhook_url\|^discord_webhook_url\|^    discord_webhook_url' | xargs | cut -d' ' -f2)
-			curl -v -i -H "Accept: application/json" -H "Content-Type: multipart/form-data" -X POST -F file1=@${1} $discord_url &>/dev/null
-		fi
-		if [[ -n "$slack_channel" ]] && [[ -n "$slack_auth" ]]; then
-			notification "Sending ${domain} data over Slack" info
-			curl -F file=@${1} -F "initial_comment=reconftw zip file" -F channels=${slack_channel} -H "Authorization: Bearer ${slack_auth}" https://slack.com/api/files.upload &>/dev/null
-		fi
+		curl --upload-file "$1" "https://transfer.sh/$1" | notify
 	fi
 }
 
