b78d51d26d90a263efd2aa9ff3bdade162347b72
six2dez <six2dez@gmail.com>
2023-01-16 10:10:54 +0100

    fix sendtozip

--- reconftw.sh
+++ reconftw.sh
@@ -1944,12 +1944,12 @@ function getElapsedTime {
 function zipSnedOutputFolder {
 	zip_name=`date +"%Y_%m_%d-%H.%M.%S"`
 	zip_name="$zip_name"_"$domain.zip"
-	(cd $dir && zip -r "../$zip_name" .)
+	(cd $dir && zip -r "$zip_name" .)
 
-	echo "Sending zip file "${dir_output}/${zip_name}""
-	if [ -s "$dir_output/$zip_name" ]; then
-		sendToNotify "$dir_output/$zip_name"
-		rm -f "$dir_output/$zip_name"
+	echo "Sending zip file "${dir}/${zip_name}""
+	if [ -s "${dir}/$zip_name" ]; then
+		sendToNotify "$dir/$zip_name"
+		rm -f "${dir}/$zip_name"
 	else
 		notification "No Zip file to send" warn
 	fi
