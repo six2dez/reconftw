d02afdc217a5d29c1031e40fb33fe2dcdfb57f2e
Will Kapcio <18176030+whiskeykilo@users.noreply.github.com>
2024-08-21 00:45:26 -0400

    Soft notification for multi_recon() start and end

--- reconftw.sh
+++ reconftw.sh
@@ -3308,6 +3308,8 @@ function recon() {
 
 function multi_recon() {
 
+	[ "$SOFT_NOTIFICATION" = true ] && echo "$(date +'%Y-%m-%d %H:%M:%S') Recon successfully started on ${multi}" | notify -silent
+
 	global_start=$(date +%s)
 
 	#[[ -n "$domain" ]] && ipcidr_target $domain
@@ -3508,6 +3510,7 @@ function multi_recon() {
 	dir=$workdir
 	domain=$multi
 	end
+	[ "$SOFT_NOTIFICATION" = true ] && echo "$(date +'%Y-%m-%d %H:%M:%S') Finished Recon on: ${domain} in ${runtime}" | notify -silent
 }
 
 function multi_custom() {
