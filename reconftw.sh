0a72d9ef1edc141ddf188b0693e20b0c9ad60a78
Daud <46789550+mrdzer0@users.noreply.github.com>
2025-02-02 22:09:17 +0700

    Fixing #929 issue
    Fixing Issue: https://github.com/six2dez/reconftw/issues/929

--- reconftw.sh
+++ reconftw.sh
@@ -3292,7 +3292,7 @@ function nuclei_check() {
 				printf "${yellow}\n[$(date +'%Y-%m-%d %H:%M:%S')] Running: Nuclei Severity: $crit ${reset}\n\n"
 
 				# Run nuclei for each severity level
-				nuclei $NUCLEI_FLAGS -severity "$crit" -nh -rl "$NUCLEI_RATELIMIT" "$NUCLEI_EXTRA_ARGS" -j -o "nuclei_output/${crit}_json.txt" <.tmp/webs_nuclei.txt
+    				nuclei $NUCLEI_FLAGS -severity "$crit" -nh -rl "$NUCLEI_RATELIMIT" "$NUCLEI_EXTRA_ARGS" <.tmp/webs_nuclei.txt | tee "${dir}/nuclei_output/${crit}.txt"
 			done
 			printf "\n\n"
 		else
